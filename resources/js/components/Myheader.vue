e42ce4da9c8f957b4c5be65fd54b9446fbb7944b
Allan-Sanchez <asanchezrixtun@gmail.com>
2019-03-04 15:39:18 -0600

    iniciando a crear component de plantilla

---
+++ resources/js/components/Myheader.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="#">
+                <img src="https://bulma.io/images/bulma-logo.png" alt="PHONEBOOK" width="112" height="28">
+            </a>
+        </div>
+        <div class="navbar-menu is-active">
+            <a class="navbar-item" href="#">Inicio</a>
+            <a class="navbar-item" href="#">Acerca de</a>
+
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        // mounted() {
+        //     console.log('Component mounted.')
+        // }
+    }
+
+</script>
---
+++ resources/js/components/footerComponet.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                <strong>Vuejs Phonebook App</strong> realizado <a href="https://github.com/Allan-Sanchez">Allan Sanchez</a>. El codigo de este proyecto lo encontraras en:
+                <a href="https://github.com/Allan-Sanchez/phonebook">Git/phonebook</a>.
+            </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        // mounted() {
+        //     console.log('Component mounted.')
+        // }
+    }
+</script>
